76a966db57e9a0abe271e4e5916a2d517acc67d3
Jean Chung <jeanchung@users.noreply.github.com>
2016-08-14 20:18:28 -0500

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,5 +1,5 @@
 def is_pangram(sentence):
-    """A function that determines whether a sentence is a pangram."""
+    """Determines whether a sentence is a pangram."""
     
     full_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     sentence = sentence.lower()
