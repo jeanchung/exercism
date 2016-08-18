5b7fdfc8e1c85a77d8d6853ae412606bbafafb53
Jean Chung <jeanchung@users.noreply.github.com>
2016-08-18 09:49:51 -0500

    Update clock.py
    Replaced loops with modulo operation and created a method for rollover.

--- clock.py
+++ clock.py
@@ -1,33 +1,11 @@
 class Clock(object):
-    """A clock is initialized to a specific time and can have minutes added or subtracted."""
-    
-    def __init__(self, hour, minute):
-        # roll over hours if greater than 23
-        while hour > 23:
-            hour -= 24
-            
-        # roll over minutes if greater than 59
-        while minute > 59:
-            minute -= 60
-            if hour == 23:
-                hour = 0;
-            else:
-                hour += 1
-        
-        # negative hours
-        while hour < 0:
-            hour += 24
-        
-        # negative minutes
-        while minute < 0:
-            minute += 60
-            if hour == 0:
-                hour = 23
-            else:
-                hour -= 1
-            
+    """A clock is initialized to a specific time.
+    It can have minutes added or subtracted."""
+
+    def __init__(self, hour, minute):            
         self.hour = hour
         self.minute = minute
+        self.rollover()
     
     def __str__(self):
         # hours are always double digits
@@ -45,32 +23,21 @@ class Clock(object):
         return displayHour + ":" + displayMinute
         
     def add(self, quant):
-        # adding minutes
-        if quant >= 0:
-            self.minute += quant
-            while self.minute > 59:
-                self.minute -= 60                
-                # roll over hours
-                if self.hour == 23:
-                    self.hour = 0
-                else:
-                    self.hour += 1
-        
-        # subtracting minutes
-        else:
-            self.minute += quant
-            while self.minute < 0:
-                self.minute += 60
-                # roll over hours
-                if self.hour == 0:
-                    self.hour = 23
-                else:
-                    self.hour -= 1
-       
+        self.minute += quant
+        self.rollover()
         return self
+    
+    def rollover(self):
+        if self.minute < 0:
+            multiples = int(self.minute / 60.0) - 1
+        else:
+            multiples = int(self.minute / 60.0)
+    
+        self.minute = self.minute % 60        
+        self.hour += multiples    
+        self.hour = self.hour % 24
         
     def __eq__(self, other):
-        """Two clocks set to the same time are equal."""
         if self.hour == other.hour and self.minute == other.minute:
             return True
         else:
