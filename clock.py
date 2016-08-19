26d39a55ead446bfe4bc556d7d776c39750c943e
Jean Chung <jeanchung@users.noreply.github.com>
2016-08-18 21:39:36 -0500

    Update clock.py
    Simplified prepending clock digits with 0 and implemented rollover method using divmod.

--- clock.py
+++ clock.py
@@ -1,26 +1,16 @@
 class Clock(object):
-    """A clock is initialized to a specific time.
-    It can have minutes added or subtracted."""
+    """
+    A clock is initialized to a specific time.
+    It can have minutes added or subtracted.
+    """
 
     def __init__(self, hour, minute):            
         self.hour = hour
         self.minute = minute
         self.rollover()
     
-    def __str__(self):
-        # hours are always double digits
-        if self.hour < 10:
-            displayHour = "0{0}".format(self.hour)
-        else:
-            displayHour = str(self.hour)
-        
-        # minutes are always double digits    
-        if self.minute < 10:
-            displayMinute = "0{0}".format(self.minute)
-        else:
-            displayMinute = str(self.minute)
-            
-        return displayHour + ":" + displayMinute
+    def __str__(self):      
+        return "{0:02d}:{1:02d}".format(self.hour, self.minute)
         
     def add(self, quant):
         self.minute += quant
@@ -28,14 +18,9 @@ class Clock(object):
         return self
     
     def rollover(self):
-        if self.minute < 0:
-            multiples = int(self.minute / 60.0) - 1
-        else:
-            multiples = int(self.minute / 60.0)
-    
-        self.minute = self.minute % 60        
-        self.hour += multiples    
+        self.hour += divmod(self.minute, 60)[0]
         self.hour = self.hour % 24
+        self.minute = self.minute % 60         
         
     def __eq__(self, other):
         if self.hour == other.hour and self.minute == other.minute:
