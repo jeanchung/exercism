c187ebe1ebaa49100b598bb7b0b3bd46aef20f8c
Jean Chung <jeanchung@users.noreply.github.com>
2016-08-18 22:54:41 -0500

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,5 +1,8 @@
 def is_pangram(sentence):
-    """Determines whether a sentence is a pangram."""
+    """
+    Determines whether a sentence is a pangram.
+    
+    Pangram: a sentence that uses every letter of the alphabet at least once."""
     
     full_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     sentence = sentence.lower()
