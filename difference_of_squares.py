feedf45f425f8f694a6771f21d4029d9fd554f93
Jean Chung <jeanchung@users.noreply.github.com>
2016-08-27 12:45:07 -0500

    Update difference_of_squares.py

--- difference_of_squares.py
+++ difference_of_squares.py
@@ -6,7 +6,7 @@ def square_of_sum(n):
     return sum * sum
 
 def sum_of_squares(n):
-    """Calculaes the square of the sum of the first n natural numbers."""
+    """Calculates the square of the sum of the first n natural numbers."""
     sum = 0
     for i in range(1, n+1):
         sum += i * i
